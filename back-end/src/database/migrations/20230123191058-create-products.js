b10b034236a1701505d5422bcbdae2cc06e089bb
Bruno Krauthamer <brunokrauthamer@gmail.com>
2023-01-23 16:58:18 -0300

    migrations

---
+++ back-end/src/database/migrations/20230123191058-create-products.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.DECIMAL,
+        allowNull: false,
+      },
+      url_image: {
+        type: Sequelize.STRING(200),
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await Sequelize.dropTable('products');
+  },
+};
---
+++ back-end/src/database/migrations/20230123191132-create-sale-product.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('sales_products', {
+      sale_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'sales',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        primaryKey: true,
+      },
+      product_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'products',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        primaryKey: true,
+      },
+      quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('sales_products');
+  },
+};
